0149f009311546bfb12ed7188d693ee564bce2b6
Lochlan Wansbrough <lochie@live.com>
2015-10-14 18:55:06 -0700

    Upgraded dependencies

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -10,10 +10,6 @@ import reducers from './reducers';
 import App from './components/App.jsx';
 import { Welcome } from './components/pages';
 
-/* Shims */
-// TODO: Remove when WinJS moves to React 0.14
-React.findDOMNode = ReactDOM.findDOMNode;
-
 let routes = (
   <Route path="/" component={App}>
     <Route path="/welcome" component={Welcome} />
